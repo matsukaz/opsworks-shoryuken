9663081ba495c81a228bae1ab8932c343e4a8d39
matsukaz <matsukaz@gmail.com>
2017-10-25 15:20:38 +0900

    create shoryuken.yml if config specified

--- recipes/deploy.rb
+++ recipes/deploy.rb
@@ -6,6 +6,25 @@ node[:deploy].each do |application, deploy|
     start_command = shoryuken_config['start_command'] || 'bundle exec shoryuken -R -L log/shoryuken.log -C config/shoryuken.yml'
     env = deploy['environment_variables'] || {}
 
+    template "#{deploy[:deploy_to]}/shared/config/shoryuken.yml" do
+      source "shoryuken.yml.erb"
+      mode "0660"
+      group deploy[:group]
+      owner deploy[:user]
+      variables :content => YAML.dump(JSON.parse(shoryuken_config[:config].to_json))
+      only_if do
+        shoryuken_config['config'].present?
+      end
+    end
+
+    execute "symlink shoryuken.yml" do
+      command "ln -s #{deploy[:deploy_to]}/shared/config/shoryuken.yml #{deploy[:deploy_to]}/current/config/shoryuken.yml"
+      action :run
+      only_if do
+        shoryuken_config['config'].present?
+      end
+    end
+
     template 'setup shoryuken.conf' do
       path "/etc/init/shoryuken-#{application}.conf"
       source 'shoryuken.conf.erb'
